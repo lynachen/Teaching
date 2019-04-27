92e4d125182656f33162483c4634374986568fec
Julyna <lingna.chen@outlook.com>
2019-04-27 22:36:14 +0800

    first commit

---
+++ assets/js/common.js
@@ -0,0 +1,4 @@
+$(function(){
+	$("#header").load("../../template/Common/common.html .common-header");
+	$("#footer").load("../../template/Common/common.html .common-footer");
+})
---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+$(function(){
+	$(".header").load("common.html .common-header");
+	$(".footer").load("common.html .common-footer");
+	
+//	$(window).on("scroll",function(){
+//		if($(window).scrollTop() == 0){
+//			$(".common-header").removeClass("fixed");
+//		} else{
+//			$(".common-header").addClass("fixed");
+//		}
+//	})
+})
