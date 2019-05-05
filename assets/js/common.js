b801307788d0820acf2b59fb6f65125891d66326
lynachen <lynachen@126.com>
2019-05-05 17:37:57 +0800

    update

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,11 +1,20 @@
 $(function(){
-	$("#header").load("../../template/Common/common.html .common-header");
+	$("#header").load("../../template/Common/common.html .common-header",function(){
+		$("#btnSearch").on("click",function(){
+			$("#navSearch").hide()
+			$("#retSearch").show()
+		})	
+		$("#btnClose").on("click",function(){
+			$("#retSearch").hide()
+			$("#navSearch").show()
+		})
+	});
 	$("#footer").load("../../template/Common/common.html .common-footer");
 })
-	$(window).on("scroll",function(){
-		if($(window).scrollTop() == 0){
-			$(".common-header").removeClass("fixed");
-		} else{
-			$(".common-header").addClass("fixed");
-		}
-	})
+$(window).on("scroll",function(){
+	if($(window).scrollTop() == 0){
+		$(".common-header").removeClass("fixed");
+	} else{
+		$(".common-header").addClass("fixed");
+	}
+})
